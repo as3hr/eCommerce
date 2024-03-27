85c96cf078b7b91c9d8e309787c546e5f56c7736
Asher <asheressani@gmail.com>
2024-03-27 09:39:12 +0500

    Image architecture completed

--- lib/helpers/functions/loading_wrapper.dart
+++ lib/helpers/functions/loading_wrapper.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:get/get.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 
--- lib/helpers/widgets/side_bar/side_bar.dart
+++ lib/helpers/widgets/side_bar/side_bar.dart
@@ -5,9 +5,7 @@ import 'package:ecommerce_admin_panel/helpers/widgets/side_bar/screen_item.dart'
 import 'package:ecommerce_admin_panel/screens/log_in_view/auth_controller.dart';
 import 'package:ecommerce_admin_panel/routes/route_name.dart';
 import 'package:ecommerce_admin_panel/theme.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
