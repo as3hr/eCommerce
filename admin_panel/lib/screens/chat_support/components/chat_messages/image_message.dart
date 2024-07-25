0dc4719f47cb615c1628b279620f23837c891fdf
Asher <asheressani@gmail.com>
2024-07-25 23:50:34 +0500

    Fixed Chat image exception

--- admin_panel/lib/screens/chat_support/components/chat_messages/image_message.dart
+++ admin_panel/lib/screens/chat_support/components/chat_messages/image_message.dart
@@ -1,4 +1,4 @@
-import 'package:ecommerce_admin_panel/helpers/widgets/shimmer_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -6,6 +6,7 @@ import 'package:get/get.dart';
 import '../../../../../../../helpers/functions/date_helper.dart';
 import '../../../../../../../models/message.dart';
 import '../../../../helpers/styles/app_colors.dart';
+import '../../../../helpers/widgets/shimmer_effect.dart';
 import '../../chat_controller.dart';
 
 class ImageMessage extends StatelessWidget {
@@ -16,56 +17,83 @@ class ImageMessage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Row(
         children: [
-          Container(
-            constraints: BoxConstraints(
-              maxWidth: 0.25.sw,
-            ),
-            decoration: const BoxDecoration(
-                color: AppColors.offWhite,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.zero,
-                  topRight: Radius.circular(12),
-                  bottomLeft: Radius.circular(12),
-                  bottomRight: Radius.circular(12),
-                )),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: GetBuilder<ChatController>(builder: (controller) {
-                    return ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: ShimmerImage(
-                          imageUrl: message.image ?? '',
-                        ));
-                  }),
-                ),
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 7),
-                      child: Text(message.text ?? ''),
-                    )),
-              ],
-            ),
-          ),
-          5.verticalSpace,
-          Row(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              5.horizontalSpace,
-              Text(DateHelper.formatDate(message.date!)),
-              5.horizontalSpace,
-              const Icon(
-                Icons.check,
-                size: 15,
+              Container(
+                padding: const EdgeInsets.all(5),
+                constraints: BoxConstraints(
+                  maxWidth: 0.25.sw,
+                ),
+                decoration: const BoxDecoration(
+                    color: AppColors.offWhite,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.zero,
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                    )),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: GetBuilder<ChatController>(builder: (controller) {
+                        return ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: CachedNetworkImage(
+                              width: 0.25.sw,
+                              height: 220,
+                              imageUrl: message.image ?? '',
+                              imageBuilder: (context, imageProvider) =>
+                                  Container(
+                                decoration: BoxDecoration(
+                                  color:
+                                      Theme.of(context).colorScheme.onPrimary,
+                                  image: DecorationImage(
+                                    image: imageProvider,
+                                    fit: BoxFit.fitWidth,
+                                    colorFilter: ColorFilter.mode(
+                                      Theme.of(context).colorScheme.onPrimary,
+                                      BlendMode.dst,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              placeholder: (context, url) =>
+                                  const ShimmerEffect(),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            ));
+                      }),
+                    ),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 7),
+                          child: Text(message.text ?? ''),
+                        )),
+                  ],
+                ),
               ),
+              5.verticalSpace,
+              Row(
+                children: [
+                  5.horizontalSpace,
+                  Text(DateHelper.formatDate(message.date!)),
+                  5.horizontalSpace,
+                  const Icon(
+                    Icons.check,
+                    size: 15,
+                  ),
+                ],
+              )
             ],
-          )
+          ),
+          const Spacer(),
         ],
       ),
     );
